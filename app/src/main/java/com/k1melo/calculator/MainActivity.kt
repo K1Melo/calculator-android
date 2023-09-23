a6649cb204cb337d5b01a9099c8454e3accc3748
Kauã Fernandes <kauafernandes.smelo@gmail.com>
2023-09-23 17:18:07 -0300

    Update all layout of the calculator

--- app/src/main/java/com/k1melo/calculator/MainActivity.kt
+++ app/src/main/java/com/k1melo/calculator/MainActivity.kt
@@ -1,31 +1,60 @@
 package com.k1melo.calculator
 
 import android.annotation.SuppressLint
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button1: Button = findViewById(R.id.button1)
-        val button0: Button = findViewById(R.id.button0)
+        this.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
 
-        addNumber(button1)
-        addNumber(button0)
+        val calculatorResult:TextView = findViewById(R.id.calculatorNumber)
+        print(calculatorResult.textSize)
+
+        val button1: Button = findViewById(R.id.number1)
+        val button2: Button = findViewById(R.id.number2)
+        val button3: Button = findViewById(R.id.number3)
+        val button4: Button = findViewById(R.id.number4)
+        val button5: Button = findViewById(R.id.number5)
+        val button6: Button = findViewById(R.id.number6)
+        val button7: Button = findViewById(R.id.number7)
+        val button8: Button = findViewById(R.id.number8)
+        val button9: Button = findViewById(R.id.number9)
+        val button0: Button = findViewById(R.id.number0)
+
+        addNumber(button1, calculatorResult)
+        addNumber(button2, calculatorResult)
+        addNumber(button3, calculatorResult)
+        addNumber(button4, calculatorResult)
+        addNumber(button5, calculatorResult)
+        addNumber(button6, calculatorResult)
+        addNumber(button7, calculatorResult)
+        addNumber(button8, calculatorResult)
+        addNumber(button9, calculatorResult)
+        addNumber(button0, calculatorResult)
     }
 
     @SuppressLint("SetTextI18n")
-    fun addNumber(button: Button) {
-        val textAbove: TextView = findViewById(R.id.numbers)
-        val buttonNumber: Int = Integer.parseInt(button.text.toString())
-
+    fun addNumber(button: Button, text: TextView) {
         button.setOnClickListener {
-
-            textAbove.text = (textAbove.text).toString() + (buttonNumber).toString()
+            if (text.length() < 18) {
+                if ((text.length() + 1) % 4 == 0) {
+                    text.text = """${(text.text)},"""
+                }
+                text.text = """${(text.text)}${(button.text)}"""
+            } else if(text.length() < 27) {
+                if ((text.length() + 1) % 4 == 0) {
+                    text.text = """${(text.text)},"""
+                }
+                text.textSize = 35F
+                text.text = """${(text.text)}${(button.text)}"""
+            }
         }
     }
-
 }
