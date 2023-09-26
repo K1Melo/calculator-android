29442354490e1156d5932ce1e76da494baff72d3
Kauã Fernandes <kauafernandes.smelo@gmail.com>
2023-09-26 16:50:50 -0300

    Oparations function finished

--- app/src/main/java/com/k1melo/calculator/MainActivity.kt
+++ app/src/main/java/com/k1melo/calculator/MainActivity.kt
@@ -6,76 +6,143 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import net.objecthunter.exp4j.Expression
+import net.objecthunter.exp4j.ExpressionBuilder
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+    @SuppressLint("MissingInflatedId", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         this.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
 
-        val calculatorResult:TextView = findViewById(R.id.calculatorNumber)
-        print(calculatorResult.textSize)
-
-        val button1: Button = findViewById(R.id.number1)
-        val button2: Button = findViewById(R.id.number2)
-        val button3: Button = findViewById(R.id.number3)
-        val button4: Button = findViewById(R.id.number4)
-        val button5: Button = findViewById(R.id.number5)
-        val button6: Button = findViewById(R.id.number6)
-        val button7: Button = findViewById(R.id.number7)
-        val button8: Button = findViewById(R.id.number8)
-        val button9: Button = findViewById(R.id.number9)
-        val button0: Button = findViewById(R.id.number0)
-
-        addNumber(button1, calculatorResult)
-        addNumber(button2, calculatorResult)
-        addNumber(button3, calculatorResult)
-        addNumber(button4, calculatorResult)
-        addNumber(button5, calculatorResult)
-        addNumber(button6, calculatorResult)
-        addNumber(button7, calculatorResult)
-        addNumber(button8, calculatorResult)
-        addNumber(button9, calculatorResult)
-        addNumber(button0, calculatorResult)
-
-        clearAll(calculatorResult)
-        clearLast(calculatorResult)
+        operations()
     }
 
-    @SuppressLint("SetTextI18n")
-    fun addNumber(button: Button, text: TextView) {
-        button.setOnClickListener {
-            if (text.length() < 15) {
+    private fun operations() {
+            val calculatorResult:TextView = findViewById(R.id.calculatorNumber)
+            val resultText:TextView = findViewById(R.id.result)
 
-                text.text = """${(text.text)}${(button.text)}"""
-            } else if(text.length() < 23) {
+            val button1: Button = findViewById(R.id.number1)
+            button1.setOnClickListener {
+                addExpression("1", calculatorResult)
+            }
 
-                text.textSize = 35F
-                text.text = """${(text.text)}${(button.text)}"""
+            val button2: Button = findViewById(R.id.number2)
+            button2.setOnClickListener {
+                addExpression("2", calculatorResult)
+            }
+
+            val button3: Button = findViewById(R.id.number3)
+            button3.setOnClickListener {
+                addExpression("3", calculatorResult)
+            }
+
+            val button4: Button = findViewById(R.id.number4)
+            button4.setOnClickListener {
+                addExpression("4", calculatorResult)
+            }
+
+            val button5: Button = findViewById(R.id.number5)
+            button5.setOnClickListener {
+                addExpression("5", calculatorResult)
+            }
+
+            val button6: Button = findViewById(R.id.number6)
+            button6.setOnClickListener {
+                addExpression("6", calculatorResult)
+            }
+
+            val button7: Button = findViewById(R.id.number7)
+            button7.setOnClickListener {
+                addExpression("7", calculatorResult)
+            }
+
+            val button8: Button = findViewById(R.id.number8)
+            button8.setOnClickListener {
+                addExpression("8", calculatorResult)
+            }
+
+            val button9: Button = findViewById(R.id.number9)
+            button9.setOnClickListener {
+                addExpression("9", calculatorResult)
+            }
+
+            val button0: Button = findViewById(R.id.number0)
+            button0.setOnClickListener {
+                addExpression("0", calculatorResult)
+            }
+
+            val buttonSum: Button = findViewById(R.id.sum)
+            buttonSum.setOnClickListener {
+                addExpression("+", calculatorResult)
+            }
+
+            val buttonSub: Button = findViewById(R.id.subtraction)
+            buttonSub.setOnClickListener {
+                addExpression("-", calculatorResult)
+            }
+
+            val buttonDiv: Button = findViewById(R.id.divide)
+            buttonDiv.setOnClickListener {
+                addExpression("/", calculatorResult)
+            }
+
+            val buttonMut: Button = findViewById(R.id.multiplication)
+            buttonMut.setOnClickListener {
+                addExpression("*", calculatorResult)
+            }
+
+            val dot: Button = findViewById(R.id.dot)
+            dot.setOnClickListener {
+                addExpression(".", calculatorResult)
+            }
+
+            clearAll(calculatorResult)
+            clearLast(calculatorResult)
+
+            val buttonEquals: Button = findViewById(R.id.equals)
+            buttonEquals.setOnClickListener {
+                try {
+                    val expression = ExpressionBuilder(calculatorResult.text.toString()).build()
+                    val result = expression.evaluate()
+                    val longResult = result.toLong()
+
+                    if (result == longResult.toDouble()) {
+                        resultText.hint = """=""" + longResult.toString()
+                    } else {
+                        resultText.hint = """=""" + result.toString()
+                    }
+                }catch (e: Exception) {
+                }
             }
         }
-    }
+
+
+    private fun addExpression(string: String, text: TextView) {
+            text.append(string)
+        }
 
     private fun clearAll(text: TextView) {
-        val buttonClearAll: Button = findViewById(R.id.clearAll)
+            val buttonClearAll: Button = findViewById(R.id.clearAll)
 
-        buttonClearAll.setOnClickListener {
-            text.text = ""
-            text.hint = "0"
-            text.textSize = 50F
-        }
-    }
-
-    @SuppressLint("SetTextI18n")
-    private fun clearLast(text: TextView) {
-        val buttonClearOne: Button = findViewById(R.id.clearOne)
-
-        buttonClearOne.setOnClickListener {
-            text.text = text.text.removeSuffix(text.text.last().toString())
-            if (text.length() < 18) {
+            buttonClearAll.setOnClickListener {
+                text.text = ""
+                text.hint = "0"
                 text.textSize = 50F
             }
         }
-    }
+
+    @SuppressLint("SetTextI18n")
+    private fun clearLast(text: TextView) {
+            val buttonClearOne: Button = findViewById(R.id.clearOne)
+
+            buttonClearOne.setOnClickListener {
+                text.text = text.text.removeSuffix(text.text.last().toString())
+                if (text.length() < 18) {
+                    text.textSize = 50F
+                }
+            }
+        }
+
 }
